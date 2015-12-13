1a5f7db2f40ce7f036d22fb40480c30386b12a7f
Ben Klein <robobenklein@gmail.com>
2015-12-13 17:19:58 -0500

    Added error handling

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
-print("YOU WIN! Congratulations!")
+import sys
+
+# Check to ensure Python is not insane
+assert True is not False
+
+try:
+  print("YOU WIN! Congratulations!")
+except:
+  sys.exit("Error Winning, please try again.")

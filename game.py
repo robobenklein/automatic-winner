45ff93266e37702e02988d33e5880048a3d3da48
Ben Klein <robobenklein@gmail.com>
2015-12-21 09:38:41 -0500

    Just in case

--- game.py
+++ game.py
@@ -5,5 +5,11 @@ assert True is not False
 
 try:
   print("YOU WIN! Congratulations!")
-except:
-  sys.exit("Error Winning, please try again.")
+except: # If this ever executes there is a real problem!
+  print("There was an error winning by Standard output, trying other win methods.")
+  try:
+    import webbrowser
+    webbrowser.open("http://icdn4.digitaltrends.com/image/a_winner_is_you_1024-1024x768.jpg", 2)t
+  except:
+    print("Could not deliver win message, you technically win still, since you've clearly beat me.")
+    print("Congrats!")
